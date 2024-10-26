db0480c779a238c4dfab5c9ea4953d0e6a46f225
Andrey Devyatkin <andrey.devyatkin@fivexl.io>
2024-10-26 16:45:28 +0100

    automate annoying manual actions
    Signed-off-by: Andrey Devyatkin <andrey.devyatkin@fivexl.io>

---
+++ do-bankinter.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -ex
+
+source env/bin/activate
+
+FILES=$(find ~/Downloads/ -name "Movimi*.xls" -print)
+
+for FILE in "${FILES}"
+do
+  python3 bankinter.py "${FILE}"
+  rm -rf "${FILE}"
+done 
