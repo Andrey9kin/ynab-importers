52b9f22712b7a3ff46acd03a793f2bd2032eff28
Andrey Devyatkin <andrey.devyatkin@fivexl.io>
2024-10-29 09:51:15 +0000

    the same for revolut
    Signed-off-by: Andrey Devyatkin <andrey.devyatkin@fivexl.io>

---
+++ do-revolut.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -ex
+
+source env/bin/activate
+
+FILES=$(find ~/Downloads/ -name "account-statement*.csv" -print)
+
+for FILE in "${FILES}"
+do
+  python3 revolut.py "${FILE}"
+  rm -rf "${FILE}"
+done
